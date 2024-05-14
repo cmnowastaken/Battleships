0f11a3bef8cfe78fcd38189d886d3d31d802d9f7
Sam <94141340+cmnowastaken@users.noreply.github.com>
2024-05-15 10:57:24 +1200

    Made the board generation work.

--- src/PrintBoard.java
+++ src/PrintBoard.java
@@ -1,16 +1,31 @@
 public class PrintBoard {
     public static void main(String[] args) {
-        int columns = 12;
-        int rows = 12;
+        int columns = 10;
+        int rows = 10;
 
         String[][] board = new String[columns][rows];
 
+        for (int i = 0; i < columns + 1; i++) {
+            System.out.print(" ABCDEFGHIJ ".charAt(i) + "  ");
+        }
+        System.out.println();
+
         for (int i = 0; i < columns; i++) {
-            System.out.print("•");
+            System.out.print(i + " ");
                 for (int j = 0; j < rows; j++) {
-                    System.out.println(" ");
+                    System.out.print(" • ");
                 }
+                System.out.println();
             }
         }
     }
+
+
+
+
+
+
+
+
+
 
